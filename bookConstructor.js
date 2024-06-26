0492d48a26716908393e60895078eda24e3313e0
Philip Dahlberg <philiparonkd@gmail.com>
2024-06-26 19:49:10 +0200

    Quick concept for adding books to library

--- bookConstructor.js
+++ bookConstructor.js
@@ -1,8 +1,10 @@
+const myLibrary = []; //Stores all books.
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    if (read === true) {
+    if (read) {
         this.read = "already read";
     } else {
         this.read = "not read yet";
@@ -10,4 +12,13 @@ function Book(title, author, pages, read) {
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
+}
+
+function addBookToLibrary() {
+    const title = prompt("What is the books title?");
+    const author = prompt("Who wrote the book?");
+    const pages = prompt("How many pages does the book contain?");
+    const read = prompt("Have you read the book yet? Type something if yes, otherwise leave empty")
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }

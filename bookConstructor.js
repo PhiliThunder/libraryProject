e3ea7715ab0d654c02c8924f2c22a6f54beb7d1e
Philip Dahlberg <philiparonkd@gmail.com>
2024-06-26 17:51:56 +0200

    Removed stray code

--- bookConstructor.js
+++ bookConstructor.js
@@ -11,16 +11,3 @@ function Book(title, author, pages, read) {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
 }
-function Player(name, marker) {
-    this.name = name;
-    this.marker = marker;
-    this.sayName = function() {
-      console.log(this.name)
-    };
-  }
-  
-  const player1 = new Player('steve', 'X');
-  const player2 = new Player('also steve', 'O');
-  player1.sayName(); // logs 'steve'
-  player2.sayName(); // logs 'also steve'
-  
